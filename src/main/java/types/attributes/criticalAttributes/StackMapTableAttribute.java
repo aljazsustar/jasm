aa7182b1d19061ad3699121b7a737dbcf1f72594
Aljaz <aljazsustar99@gmail.com>
2023-02-28 17:55:58 +0100

    Added VerificationTypeInfo

--- src/main/java/types/attributes/criticalAttributes/StackMapTableAttribute.java
+++ src/main/java/types/attributes/criticalAttributes/StackMapTableAttribute.java
@@ -6,6 +6,8 @@ import interfaces.ClassFileElement;
 import java.util.List;
 
 public class StackMapTableAttribute extends AttributeBase implements ClassFileElement {
+
+
     @Override
     public List<Byte> toHex() {
         return null;
