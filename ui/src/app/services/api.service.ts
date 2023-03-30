775b92ccaa11ab58ea1fdbb5c57c3cfe7c118c05
Aljaz <aljazsustar99@gmail.com>
2023-03-30 11:29:18 +0200

    Fixed api url

--- ui/src/app/services/api.service.ts
+++ ui/src/app/services/api.service.ts
@@ -1,13 +1,12 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Result} from "./models/classFile";
-import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  private apiUrl: string = `${environment.apiUrl}/v1/compile`;
+  private apiUrl: string = `http://localhost:8080/v1/compile`;
 
   constructor(private http: HttpClient) {
   }
