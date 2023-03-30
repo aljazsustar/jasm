741578f53b56da68a676e52256630fb950486d24
Aljaz <aljazsustar99@gmail.com>
2023-03-30 11:14:58 +0200

    Added environments for prod and dev

--- ui/src/app/services/api.service.ts
+++ ui/src/app/services/api.service.ts
@@ -1,22 +1,18 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {HttpClient} from "@angular/common/http";
 import {Result} from "./models/classFile";
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  private headers = new HttpHeaders({'Content-Type': 'text/plain'});
-  private apiUrl: string = 'http://localhost:8080/v1/compile';
+  private apiUrl: string = `${environment.apiUrl}/v1/compile`;
 
   constructor(private http: HttpClient) {
   }
 
   compileSource(fileContent?: string) {
     return this.http.post<Result>(`${this.apiUrl}/getInfo`, fileContent);
-  }
-
-  getClassFile() {
-    return this.http.get<any>(`${this.apiUrl}/classFile`);
   }
 }
